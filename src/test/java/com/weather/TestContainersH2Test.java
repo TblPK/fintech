d2c542ca6970d2469f25e7105474404aba7ffcb9
TblPK <sapaevmurat735@gmail.com>
2023-11-15 17:00:08 +0300

    fix hw

--- src/test/java/com/weather/TestContainersH2Test.java
+++ src/test/java/com/weather/TestContainersH2Test.java
@@ -20,7 +20,6 @@ public class TestContainersH2Test {
     @DynamicPropertySource
     static void setProperties(DynamicPropertyRegistry registry) {
         registry.add("spring.datasource.url", () -> "jdbc:h2:tcp://localhost:" + h2.getMappedPort(1521) + "/test");
-        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n" + "localhost:" + h2.getMappedPort(81) + "/test" + "\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
         registry.add("spring.datasource.username", () -> "sa");
         registry.add("spring.datasource.password", () -> "");
     }
